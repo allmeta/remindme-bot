8f0d5571a7101829f08fa95549b4eb7d1f87b51e
Allmeta <thomasdasd@gmail.com>
2019-05-22 01:26:39 +0200

    ads id on startup for jobs too now

--- cogs/remind.py
+++ cogs/remind.py
@@ -40,6 +40,7 @@ class Remind(commands.Cog):
                                        next_run_time=datetime.strptime(
                                            job["date"],
                                            self.dfrmt),
+                                       id=jobid
                                        args=[
                                            userid,
                                            job["desc"],
